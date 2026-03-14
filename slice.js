a88341cc3e4331bb15f78e294c0a7e2ff235f68e
Archi Misra <misraarchi117@gmail.com>
2026-03-15 00:00:30 +0530

    Add files via upload

---
+++ slice.js
@@ -0,0 +1,15 @@
+let wor = "archi misra";
+console.log(wor.slice(1,3));
+
+let wor1 = "archi";
+let wor2 = "misra";
+
+let res = wor1.concat(wor2);
+console.log(res);
+
+ let string = "helllolllo";
+ console.log(string.replace("h","p"));
+
+ let x = "IloveJS";
+ console.log(x.charAt(3));
+
